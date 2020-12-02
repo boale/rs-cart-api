2f55f7e57662c23cc2a05ff3c25627ae9a2b6581
Alexander Bondarenko <bondarenko.alexander.13@gmail.com>
2020-12-02 17:06:23 +0200

    chore: disable auth on cart andpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
 
   constructor(private authService: AuthService) {}
 
-  @Get('ping')
+  @Get([ '', 'ping' ])
   healthCheck(): any {
     return {
       statusCode: HttpStatus.OK,
