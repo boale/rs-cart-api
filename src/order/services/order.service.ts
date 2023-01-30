2ba7a900a5e22f15de91b5864acf12585f2b8469
Mikhail Shumski <Mikhail_Shumski@epam.com>
2023-01-30 09:51:39 +0300

    task8

--- src/order/services/order.service.ts
+++ src/order/services/order.service.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { v4 } from 'uuid';
-
 import { Order } from '../models';
 
 @Injectable()
 export class OrderService {
-  private orders: Record<string, Order> = {}
+  private orders: Record<string, Order> = {};
 
   findById(orderId: string): Order {
-    return this.orders[ orderId ];
+    return this.orders[orderId];
   }
 
   create(data: any) {
-    const id = v4(v4())
+    const id = v4(v4());
     const order = {
       ...data,
       id,
       status: 'inProgress',
     };
 
-    this.orders[ id ] = order;
+    this.orders[id] = order;
 
     return order;
   }
@@ -31,9 +30,13 @@ export class OrderService {
       throw new Error('Order does not exist.');
     }
 
-    this.orders[ orderId ] = {
+    this.orders[orderId] = {
       ...data,
       id: orderId,
-    }
+    };
+  }
+
+  async getOrders() {
+    return { many: `A lot of orders!` };
   }
 }
